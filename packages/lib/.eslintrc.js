9d3cf22c3389265b589970074db53d6958e983c9
Kardamitsis Evan <kardamitsis.e@gmail.com>
2025-07-15 15:57:06 +0300

    Fix Vercel deployment configuration and ESLint configs

--- packages/lib/.eslintrc.js
+++ packages/lib/.eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-  extends: ["../config/eslint-preset.js"],
+  extends: ["eslint:recommended", "@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "no-console": "off",
   },
 };
